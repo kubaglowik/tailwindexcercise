8c5d425c4021e4a3af87fba8828df68aae72ff45
kubaglowik <jakubgo45@gmail.com>
2023-10-16 10:38:33 +0200

    addedd navbar

--- tailwindex/src/App.jsx
+++ tailwindex/src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-} 
+import { Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomerReviews,Subscribe,footer } from './sections'
+import Nav from './components/Nav';
+
+const app   = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl: padding-1 wide: padding-r padding-b"><Hero /></section>
+    <section className="padding" ><PopularProducts /></section>
+    <section className="padding" ><SuperQuality /></section>
+    <section className="padding-x py-10" ><Services /></section>
+    <section className="padding" ><SpecialOffers /></section>
+    <section className="bg-pale-blue padding" ><CustomerReviews /></section>
+    <section className="padding-x sm:py-32 py-16 w-full" ><Subscribe /></section>
+    <section className="padding-x padding-t bg-black pb-8" ><footer /></section>
+  </main>
+);
+export default app; 
