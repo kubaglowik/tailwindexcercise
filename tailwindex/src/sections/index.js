56f869cf1c39847fa67eac08888902a62f52a1c1
kubaglowik <jakubgo45@gmail.com>
2023-10-16 10:40:07 +0200

    import from yt

---
+++ tailwindex/src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import CustomerReviews from "./customerrevies";
+import footer from "./footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    footer,
+};
