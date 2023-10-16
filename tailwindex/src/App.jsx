79699883052957f22eeac9ba1be64805b373d63c
kubaglowik <jakubgo45@gmail.com>
2023-10-16 14:35:34 +0200

    delete parts of page

--- tailwindex/src/App.jsx
+++ tailwindex/src/App.jsx
@@ -1,17 +1,12 @@
-import { Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomerReviews,Subscribe,footer } from './sections'
 import Nav from './components/Nav';
-
+import { Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomerReviews,Subscribe,Footer } from './sections'
 const app   = () => (
   <main className="relative">
-    <Nav />
-    <section className="xl: padding-1 wide: padding-r padding-b"><Hero /></section>
+    <Nav/>
+    <section className="xl: padding-1 wide: padding-r padding-b"><Hero/></section>
     <section className="padding" ><PopularProducts /></section>
-    <section className="padding" ><SuperQuality /></section>
     <section className="padding-x py-10" ><Services /></section>
-    <section className="padding" ><SpecialOffers /></section>
-    <section className="bg-pale-blue padding" ><CustomerReviews /></section>
-    <section className="padding-x sm:py-32 py-16 w-full" ><Subscribe /></section>
-    <section className="padding-x padding-t bg-black pb-8" ><footer /></section>
+    <section className="padding-x padding-t bg-black pb-8" ><Footer /></section>
   </main>
 );
 export default app; 
